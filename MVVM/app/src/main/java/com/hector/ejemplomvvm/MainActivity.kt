a7560f54b2bc1f44ab0bdff680b6a86023b5bbb2
hector martinez <mejia.martinez2012@hotmail.es>
2021-04-27 18:53:12 -0600

    Definiendo la arquitectura MVVM
    Se agregan los packages para ir definiendo la arquitectura

--- MVVM/app/src/main/java/com/hector/ejemplomvvm/MainActivity.kt
+++ MVVM/app/src/main/java/com/hector/ejemplomvvm/MainActivity.kt
@@ -2,10 +2,33 @@ package com.hector.ejemplomvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import com.hector.ejemplomvvm.R
+import com.hector.ejemplomvvm.databinding.ActivityMainBinding
+import com.hector.ejemplomvvm.viewmodel.QuoteViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val quoteViewModel: QuoteViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        quoteViewModel.quoteModel.observe(this, Observer {
+            binding.tvQuote.text = it.quote
+            binding.tvAuthor.text = it.author
+        })
+
+
+        binding.viewContainer.setOnClickListener { quoteViewModel.randomQuote() }
+
     }
 }
